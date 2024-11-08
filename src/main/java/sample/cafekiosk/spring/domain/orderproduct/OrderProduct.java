fdf5d69eab29270b1b358eddb6da5963b47109b2
Jisu Lee <jisulee.shsf@gmail.com>
2024-11-08 21:53:47 +0900

    Business Layer 테스트2

--- src/main/java/sample/cafekiosk/spring/domain/orderproduct/OrderProduct.java
+++ src/main/java/sample/cafekiosk/spring/domain/orderproduct/OrderProduct.java
@@ -18,11 +18,11 @@ public class OrderProduct extends BaseEntity {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "order_id")
+//    @JoinColumn(name = "order_id")
     private Order order;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "product_id")
+//    @JoinColumn(name = "product_id")
     private Product product;
 
     public OrderProduct(Order order, Product product) {
